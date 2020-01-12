7efb561f238fe1acc29f4b029b0c6e19d96e6f20
Tomasz Baslyk <tomekbaslyk@gmail.com>
2020-01-12 19:13:37 +0100

    fixed progress bar bug

--- DatingApp/Scripts/profiles.js
+++ DatingApp/Scripts/profiles.js
@@ -7,16 +7,16 @@ function loadProgressBars() {
 
     var cSharpValue = $('#progress-bar-csharp').data('value') * 10;
 
-    $('#progress-bar-csharp').animate({ width: cSharpValue }, 100);
+    $('#progress-bar-csharp').animate({ width: cSharpValue + "%" }, 100);
     $('#progress-csharp').delay(1000).fadeOut(500);
 
     var javascriptValue = $('#progress-bar-javascript').data('value') * 10;
 
-    $('#progress-bar-javascript').animate({ width: javascriptValue }, 100);
+    $('#progress-bar-javascript').animate({ width: javascriptValue + "%" }, 100);
     $('#progress-javascript').delay(1000).fadeOut(500);
 
     var stackOverflowValue = $('#progress-bar-stackoverflow').data('value') * 10;
 
-    $('#progress-bar-stackoverflow').animate({ width: stackOverflowValue }, 100);
+    $('#progress-bar-stackoverflow').animate({ width: stackOverflowValue + "%" }, 100);
     $('#progress-stackoverflow').delay(1000).fadeOut(500);
 }
